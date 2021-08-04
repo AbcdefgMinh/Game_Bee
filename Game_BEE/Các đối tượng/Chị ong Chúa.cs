da7d9aab3dfa18f3dc45b70e33095d7942250952
Admin <Admin@QUANG-MINH>
2021-08-04 21:15:03 +0700

    Add project files.

---
+++ Game_BEE/Các đối tượng/Chị ong Chúa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_BEE.Các_đối_tượng
+{
+    class Chị_ong_Chúa
+    {
+
+    }
+}
---
+++ Game_BEE/Các đối tượng/Ong đực.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_BEE.Các_đối_tượng
+{
+    class Ong_đực
+    {
+    }
+}
